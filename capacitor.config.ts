d70c3fd997fd2f1b0847a670966a1164bb995866
Alexzander Ciminillo <ciminillo@Alexzanders-MBP.lan>
2022-10-31 00:01:44 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Path of Peril',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
